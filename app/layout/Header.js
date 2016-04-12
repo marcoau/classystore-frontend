f73c59fb5ad2d9aaaabc8294b5474a9115f6ec94
Marco Au <marco.au.321@gmail.com>
2016-04-12 23:14:26 +0800

    add button styling

--- app/layout/Header.js
+++ app/layout/Header.js
@@ -8,16 +8,31 @@ export default class Header extends Component {
       <div className='header'>
         <h1>Classy Store</h1>
         <div className='header-left'>
-          <a href>Fashion</a>
-          <a href>Beauty</a>
-          <a href>Taste</a>
+          <a className='button' href>
+            <i className='fa fa-shopping-bag' aria-hidden={true}></i>
+            時裝潮物
+          </a>
+          <a className='button' href>
+            <i className='fa fa-cutlery' aria-hidden={true}></i>
+            美食佳餚
+          </a>
+          <a className='button' href>
+            <i className='fa fa-flask' aria-hidden={true}></i>
+            美容扮靚
+          </a>
         </div>
         <div className='header-search'>
           <input placeholder='Search for products' />
         </div>
         <div className='header-right'>
-          <button>Login</button>
-          <button>Cart</button>
+          <button className='_bordered _fb'>
+            <i className='fa fa-facebook-square' aria-hidden={true}></i>
+              FB登入
+          </button>
+          <button>
+            <i className='fa fa-shopping-cart' aria-hidden={true}></i>
+            購物車
+          </button>
         </div>
       </div>
     );
