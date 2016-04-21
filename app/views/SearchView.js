73b3191d2123947ed04bc2b841c03774ed830e53
Marco Au <marco.au.321@gmail.com>
2016-04-21 22:27:08 +0800

    add filtering product by category

--- app/views/SearchView.js
+++ app/views/SearchView.js
@@ -4,37 +4,67 @@ import React, { Component } from 'react';
 import Product from './../components/Product';
 
 export default class SearchView extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    this._productsRef = new Firebase(`https://classy-store.firebaseio.com/products`);
+    this.pullProducts(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.pullProducts(nextProps);
+  }
+
+  pullProducts(props) {
+    const { location } = props;
+    const query = location.query;
+
+    let refQuery;
+    if(query.cat) {
+      refQuery = this._productsRef.orderByChild('category').equalTo(query.cat);
+
+    } else {
+      refQuery = this._productsRef.orderByChild('category');
+    }
+
+    refQuery.once('value', dataSnapshot => {
+      const data = dataSnapshot.val();
+      const newProducts = [];
+
+      for(let id in data) {
+        newProducts.push(data[id]);
+      }
+
+      this.setState({
+        products: newProducts,
+      });
+    });
+  }
+
   render() {
     const { params, location } = this.props;
     const query = location.query;
 
-    const CatHeader = query.cat ? 
+    const CatHeader = query.cat ?
       ( <h3>{query.cat}類</h3> ) : null;
 
     const QHeader = query.q ?
-      ( <h3>找到與「{query.q}」相關的產品</h3> ) : null;    
+      ( <h3>找到與「{query.q}」相關的產品</h3> ) : null;
+
+    const Products = this.state.products.map(p => ( <Product key={p.id} product={p} /> ));
 
     return (
       <div>
         <div className='container'>
           {CatHeader}
           {QHeader}
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+          {Products}
         </div>
       </div>
     );
