b8df70ac7b3ffde258a4e59b869fbe21459a3b9a
Marco Au <marco.au.321@gmail.com>
2016-05-01 19:05:03 +0800

    add dynamic filters

--- app/components/Filter.js
+++ app/components/Filter.js
@@ -4,33 +4,63 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 export default class Filter extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      brands: []
+    };
+  }
+
   buildQueryString(query) {
-    let queryString = '';
+    const queries = [];
+
     for(let param in query) {
-      queryString += `&${param}=${query[param]}`;
+      queries.push(`${param}=${query[param]}`);
     }
 
-    return queryString;
+    return queries.length ? queries.join('&') : '';
+  }
+
+  componentDidMount() {
+    this._brandsRef = new Firebase(`https://classy-store.firebaseio.com/brands`);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.pullFilters(nextProps);
+  }
+
+  pullFilters(props) {
+    const { query } = props;
+
+    let refQuery = this._brandsRef.orderByChild('category');
+    if(query.category) refQuery = refQuery.equalTo(query.category);
+
+    refQuery.once('value', dataSnapshot => {
+      const data = dataSnapshot.val();
+      const brands = [];
+
+      for(let id in data) {
+        const brand = data[id];
+        brands.push(brand);
+      }
+
+      this.setState({
+        brands: brands,
+      });
+    });
   }
 
   render() {
     const currentQuery = this.props.query;
 
-    let currentQueryString = '';
-    for(let param in currentQuery) {
-      currentQueryString += `&${param}=${currentQuery[param]}`;
-    }
-
-    // TEMP: hardcode brands for now
-    const BRANDS = ['Coach', 'Longchamp'];
-
-    const filterLinks = BRANDS.map(b => {
-      const newQuery = Object.assign(currentQuery, { brand: b });
+    const brandFilters = this.state.brands.map(b => {
+      const newQuery = Object.assign(currentQuery, { brand: b.name });
 
       return (
-        <p key={b}>
+        <p key={b.id} className='filter-group-link'>
           <Link to={`/search?${this.buildQueryString(newQuery)}`}>
-            {b}
+            {b.name}
           </Link>
         </p>
       );
@@ -38,8 +68,14 @@ export default class Filter extends Component {
 
     return (
       <div className='filter'>
-        <h4>品牌</h4>
-        {filterLinks}
+        <h3>分類搜尋</h3>
+        <div className='filter-group'>
+          <h4>品牌</h4>
+          {brandFilters}
+        </div>
+        <div className='filter-group'>
+          <h4>類別</h4>
+        </div>
       </div>
     );
   }
